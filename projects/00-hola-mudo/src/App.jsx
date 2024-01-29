d9581894755215acb7230a60054915bfb43eabba
fabian <fabianforero779@gmail.com>
2024-01-29 08:49:00 -0500

    Add: First class react
    First component TwitterFollowCard to learn react.

---
+++ projects/00-hola-mudo/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Angel",
+    isFollowing: true,
+  },
+  {
+    userName: "fb779",
+    name: "Fabian Forero",
+    isFollowing: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}>
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </div>
+  );
+}
+
+export default App;
