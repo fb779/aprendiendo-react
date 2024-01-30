9a0ed0745f23ce4a243197509873f5aa0c1c5620
fabian <fabianforero779@gmail.com>
2024-01-29 22:16:11 -0500

    Add: FollowMouse app
    Project to an element to follow the mouse.

---
+++ projects/02-mouse-follower/src/App.jsx
@@ -0,0 +1,12 @@
+import { FolloweMouse } from "./components/FollowMouse";
+// import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <FolloweMouse />
+    </main>
+  );
+}
+
+export default App;
---
+++ projects/02-mouse-follower/src/components/FollowMouse.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+export function FolloweMouse() {
+  const [enabled, setEnabled] = useState(false);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  /**
+   * useEfect(callback,[])
+   * undefine : se activa cuando se renderiza el componente
+   * []       : se ejecuta una vez cuando se monta el componenete
+   * [value]  : se ejecuta cuando cada vez que cambia el estado de la prop
+   */
+
+  // pointer move
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientY, clientX } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    /**
+     * cleanup:
+     * ==> cuando el componente se desmonta
+     * ==> cuando cambian las dependencias, antes de ejecutar el efecto nuevamente
+     */
+    return () => {
+      // cleanup
+      setPosition({ x: 0, y: 0 });
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+}
